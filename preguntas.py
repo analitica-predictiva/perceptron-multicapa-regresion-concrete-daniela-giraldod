1dfc41f3a919d8266d1fcccb4fbde733bb516505
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-12-03 01:44:02 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -83,8 +83,7 @@ def pregunta_04():
     Creación de la malla de búsqueda
     """
 
-    # Importe GridSearchCV
-    from sklearn.model_selection import GridSearchCV
+     from sklearn.model_selection import GridSearchCV
 
     # Cree una malla de búsqueda para el objecto GridSearchCV
     # con los siguientes parámetros de búesqueda:
@@ -97,13 +96,13 @@ def pregunta_04():
     #   * Use parada temprana
 
     param_grid = {
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
+        "mlpregressor__hidden_layer_sizes": range(1,8),  
+        "mlpregressor__activation": ["relu"],  
+        "mlpregressor__learning_rate_": ["adaptative"],  
+        "mlpregressor__momentum": (0.7,0.8,0.9),  
+        "mlpregressor__learning_rate_init": (0.01, 0.05, 0.1),  
+        "mlpregressor__max_iter": [5000],  
+        "mlpregressor__early_stopping": [True],  
     }
 
     estimator = pregunta_03()
@@ -115,11 +114,12 @@ def pregunta_04():
     gridsearchcv = GridSearchCV(
         estimator=estimator,
         param_grid=param_grid,
-        ___ = ____  
-        ___ = ____  
+        cv = 5 , 
+        scoring = 'r2'  
     )
 
     return gridsearchcv
+
 
 
 def pregunta_05():
@@ -128,7 +128,7 @@ def pregunta_05():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Cargue las variables.
     x_train, x_test, y_train, y_test = pregunta_02()
@@ -140,17 +140,17 @@ def pregunta_05():
     estimator.fit(x_train, y_train)  #
 
     # Pronostique para las muestras de entrenamiento y validacion
-    y_trian_pred = ____.____(____)  
-    y_test_pred = ____.____(____)  
+    y_trian_pred = x_train.pred(x_train)  
+    y_test_pred = x_test.pred(x_test)  
 
     # Calcule el error cuadrático medio de las muestras
-    mse_train = ____(  
-        ___,  
-        ___,  
+    mse_train = mean_squared_error(  
+        y_train,  
+        y_trian_pred,  
     )
-    mse_test = ____(  
-        ___,  
-        ___,  
+    mse_test = mean_squared_error(  
+        y_test,  
+        y_test_pred,  
     )
 
     # Retorne el mse de entrenamiento y prueba
